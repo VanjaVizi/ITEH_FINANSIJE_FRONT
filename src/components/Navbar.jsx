fd1b0e7e5ed5262f21c6e9e788b3a3524dc9694f
VanjaVizi <vanjazi999@gmail.com>
2025-11-26 00:02:53 +0100

    stranica za registraciju + logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,68 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
 
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import api from "../api/api";
+import "./Navbar.css";
 const Navbar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [isAuth, setIsAuth] = useState(false);
+
+  // svaki put kad se promeni ruta, proveri da li postoji token
+  useEffect(() => {
+    const token =
+      localStorage.getItem("token") || sessionStorage.getItem("token");
+    setIsAuth(!!token);
+  }, [location]);
+
+  const handleLogout = async () => {
+    try {
+       
+      await api.post("/logout");
+    } catch (err) {
+      console.error("Greška pri logout-u:", err);
+      // čak i ako padne poziv, svejedno ćemo da očistimo storage
+    } finally {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+
+      setIsAuth(false);
+      navigate("/login");
+    }
+  };
+
   return (
-    <div>
-        <Link to="/" className="href">Pocetna</Link>
-        <Link to="/login" className="href">Login</Link> 
-        <Link to="/register" className="href">Registracija</Link> 
+    <div className="navbar">
+      <Link to="/" className="href">
+        Pocetna
+      </Link>
 
+      {isAuth ? (
+        <>
+          <Link to="/mojefinansije" className="href">
+            Moje finansije
+          </Link>
+          <button
+            type="button"
+            className="href href-button"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <Link to="/login" className="href">
+            Login
+          </Link>
+          <Link to="/register" className="href">
+            Registracija
+          </Link>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
